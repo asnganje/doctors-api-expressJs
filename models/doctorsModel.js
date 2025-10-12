6134b2ede4470f7049955e4fc9459a63d8706d45
asnganje <abd.nganje@gmail.com>
2025-10-12 08:45:34 +0300

    add doctor schema

---
+++ models/doctorsModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const DoctorSchema = new mongoose.Schema({
+  name: {
+    type:String,
+    required: [true, 'Please provide name']
+  },
+  specialization: {
+    type:String,
+    required: [true, 'Please provide specialization'],
+    minlength:[8, 'Specialization must be at least 8 characters long']
+  },
+  biography: {
+    type: String,
+    required: [true, 'Please provide biography']
+  },
+  picture: {
+    type:String,
+    required: [true, 'Please provide picture']
+  },
+},
+{ timestamps: true }
+)
+
+export default mongoose.model('Doctor', DoctorSchema)

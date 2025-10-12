84e29cc9118ac5e1a48cf3b6536a5425fa11fc30
asnganje <abd.nganje@gmail.com>
2025-10-12 07:55:14 +0300

    configure and run basic server setup

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT
+
+const start = () => {
+  try {
+    app.listen(port, ()=> {
+      console.log(`Server is listening on port ${port}`);
+      
+    })
+  } catch (error) {
+    console.log(error);    
+  }
+}
+
+start()

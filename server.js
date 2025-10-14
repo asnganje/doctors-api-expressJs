c6571f383b89ca87a51fcf3c9fc8da58c8b6d1df
asnganje <abd.nganje@gmail.com>
2025-10-14 13:15:07 +0300

    pass the router to main app

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./db/connect.js"
 import doctorsRouter from "./routes/doctorsRouter.js"
+import authRouter from "./routes/authRouter.js"
 
 dotenv.config()
 
@@ -11,6 +12,7 @@ const url = process.env.MONGO_URI
 
 app.use(express.json())
 app.use('/api/v1/doctors', doctorsRouter)
+app.use('/api/v1/doctors/auth', authRouter)
 
 const start = async () => {
   try {

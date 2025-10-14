21bf1934071ccef86ff4dca24d5c6d5faa077144
asnganje <abd.nganje@gmail.com>
2025-10-14 13:14:10 +0300

    setup basic user schema

---
+++ models/UserModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    minlength: [8, 'Name must be at least 10 characters long'],
+    required:[true, 'Fullname is required']
+  },
+  email: {
+    type: String,
+    required:[true, 'Email is required']
+  },
+  password: {
+    type: String,
+    required:[true, 'Password is required']
+  },
+  role: {
+    type:String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  }
+})
+
+export default mongoose.model('User', UserSchema)

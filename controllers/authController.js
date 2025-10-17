d103ba2b584acc63aa08f5b0d2f9325043da4ddc
asnganje <abd.nganje@gmail.com>
2025-10-17 06:11:34 +0300

    pass hashing logic to register function

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,13 @@
 import { StatusCodes } from "http-status-codes"
 import User from "../models/UserModel.js"
+import { passwordHash} from "../utils/passwordHash.js"
 
 export const register = async (req,res) => {
   const isFirst = await User.countDocuments() === 0
   req.body.role = isFirst?"admin":"user"
+  req.body.password = await passwordHash(req.body.password)
   const user = await User.create(req.body)
-  res.status(StatusCodes.CREATED).json({msg:'registration successful', data: user})
+  res.status(StatusCodes.CREATED).json({msg:'registration successful'})
 }
 
 export const login = (req,res) => {

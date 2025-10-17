0283364cd9f191e5d6be8f142ec6f2e78da33b1a
asnganje <abd.nganje@gmail.com>
2025-10-17 11:43:40 +0300

    add verification token

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -1,8 +1,13 @@
 import jwt from "jsonwebtoken"
 
-export const createJWT = (payload) => {
+export const createJWT = (payload) => { 
   const secret = process.env.JWT_SECRET
   const exp = process.env.JWT_EXPIRY
   const token = jwt.sign(payload, secret, {expiresIn: exp})
   return token
+}
+
+export const verifyToken = (token) => {
+  const decoded = jwt.verify(token, process.env.JWT_SECRET)
+  return decoded
 }

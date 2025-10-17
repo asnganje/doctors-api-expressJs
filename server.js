9d773c001a0f52e5fd55ff3ff8043aff8c3a2173
asnganje <abd.nganje@gmail.com>
2025-10-17 08:05:17 +0300

    pass async errors package to app

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-import express from "express"
-import dotenv from "dotenv"
-import connectDB from "./db/connect.js"
-import doctorsRouter from "./routes/doctorsRouter.js"
-import authRouter from "./routes/authRouter.js"
+import express from "express";
+import "express-async-errors";
+import dotenv from "dotenv";
+import connectDB from "./db/connect.js";
+import doctorsRouter from "./routes/doctorsRouter.js";
+import authRouter from "./routes/authRouter.js";
+import errorHandler from "./middleware/errorRouteHandler.js";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const port = process.env.PORT
-const url = process.env.MONGO_URI
+const app = express();
+const port = process.env.PORT;
+const url = process.env.MONGO_URI;
 
-app.use(express.json())
-app.use('/api/v1/doctors', doctorsRouter)
-app.use('/api/v1/doctors/auth', authRouter)
+app.use(express.json());
+app.use("/api/v1/doctors", doctorsRouter);
+app.use("/api/v1/doctors/auth", authRouter);
+app.use(errorHandler);
 
 const start = async () => {
   try {
-    await connectDB(url)
-    app.listen(port, ()=> {
+    await connectDB(url);
+    app.listen(port, () => {
       console.log(`Server is listening on port ${port}`);
-      
-    })
+    });
   } catch (error) {
-    console.log(error);    
+    console.log(error);
   }
-}
+};
 
-start()
+start();

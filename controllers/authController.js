569f08126e8abd951d55bd5e28ec18ded60fb107
asnganje <abd.nganje@gmail.com>
2025-10-17 08:05:42 +0300

    utilise password compare and customError function in auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,30 @@
-import { StatusCodes } from "http-status-codes"
-import User from "../models/UserModel.js"
-import { passwordHash} from "../utils/passwordHash.js"
+import { StatusCodes } from "http-status-codes";
+import User from "../models/UserModel.js";
+import { passwordHash } from "../utils/passwordHash.js";
+import { passwordCompare } from "../utils/passwordCompare.js";
+import { CustomError } from "../middleware/customErrorHandler.js";
 
-export const register = async (req,res) => {
-  const isFirst = await User.countDocuments() === 0
-  req.body.role = isFirst?"admin":"user"
-  req.body.password = await passwordHash(req.body.password)
-  const user = await User.create(req.body)
-  res.status(StatusCodes.CREATED).json({msg:'registration successful'})
-}
+export const register = async (req, res) => {
+  const isFirst = (await User.countDocuments()) === 0;
+  req.body.role = isFirst ? "admin" : "user";
+  req.body.password = await passwordHash(req.body.password);
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ msg: "registration successful" });
+};
 
-export const login = (req,res) => {
-  res.status(StatusCodes.OK).json({msg:'login successful'})
-}
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new CustomError("User not found", StatusCodes.NOT_FOUND);
+  }
+  const isMatch = await passwordCompare(password, user.password);
+  if (!isMatch) {
+    throw new CustomError("Invalid credentials", StatusCodes.UNAUTHORIZED);
+  }
+  res.status(StatusCodes.OK).json({ msg: "login successful" });
+};
 
-export const logout = (req,res) => {
-  res.status(StatusCodes.OK).json({msg:'Logout successful'})
-}
+export const logout = (req, res) => {
+  res.status(StatusCodes.OK).json({ msg: "Logout successful" });
+};

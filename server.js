b4f2dcf918ea2186147d6ab49f468220e86ac23f
asnganje <abd.nganje@gmail.com>
2025-10-20 06:26:22 +0300

    pass ai routing logic to app

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import doctorsRouter from "./routes/doctorsRouter.js";
 import authRouter from "./routes/authRouter.js";
 import errorHandler from "./middleware/errorRouteHandler.js";
 import tokenRouter from "./routes/tokenRouter.js";
+import aiRouter from "./routes/openaiRouter.js";
 
 dotenv.config();
 
@@ -25,6 +26,7 @@ app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/doctors", doctorsRouter);
 app.use("/refresh-token", tokenRouter)
+app.use("/api/v1/ai", aiRouter)
 app.use(errorHandler);
 
 const start = async () => {
